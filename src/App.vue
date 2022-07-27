3d616fd1ecd3a1a32ba07ca0b6b032272eda3e2e
Bhumik Kalola21 <bhumik.kalola@bacancy.com>
2022-07-27 18:08:48 +0530

    Login with google api done

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,79 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+    <b-card>
+      <b-icon
+        icon="google"
+        v-if="!gisLogin"
+        @click="login()"
+        class="icon1"
+      ></b-icon>
+      <b-icon
+        v-else-if="gisLogin"
+        @click="logOut()"
+        icon="power"
+        aria-hidden="true"
+      ></b-icon>
+      <b-icon v-if="!gisLogin" icon="linkedin" class="icon1"></b-icon>
+    </b-card>
+    <div>
+      <b-card
+        v-if="gisLogin"
+        no-body
+        class="card2 overflow-hidden"
+        style="max-width: 540px"
+      >
+        <b-row no-gutters>
+          <b-col md="3">
+            <b-card-img
+              :src="this.showResult.vO"
+              alt="Image"
+              class="rounded-0"
+            ></b-card-img>
+          </b-col>
+          <b-col md="9">
+            <b-card-text>
+              <div>
+                <b>Full Name: {{ this.showResult.Af }}</b>
+              </div>
+              <div>
+                <b>e-mail:{{ this.showResult.gw }}</b>
+              </div>
+            </b-card-text>
+          </b-col>
+        </b-row>
+      </b-card>
+    </div>
   </div>
 </template>
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      gisLogin: false,
+      lisLogin: false,
+      showResult: {},
+    };
+  },
+  methods: {
+    async login() {
+      const googleUser = await this.$gAuth.signIn();
 
+      // console.log("getBaseProfile", googleUser.getBasicProfile());
+      (this.showResult = googleUser.getBasicProfile()),
+        console.log(this.showResult),
+        console.log(this.showResult.vO);
+        (this.gisLogin = true),
+        (this.gisLogin = this.$gAuth.isAuthorized);
+    },
+    async logOut() {
+      const result = await this.$gAuth.signOut();
+      this.gisLogin = false;
+      console.log(`result`, result);
+    },
+  },
+};
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -15,18 +81,16 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  margin-top: 60px;
+  padding: 10rem;
+}
+.icon1 {
+  padding: 20px;
 }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+.card2 {
+  margin: auto;
+  height: 100px;
+  width:100%;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import { BIcon } from 'bootstrap-vue'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
 
+import GAuth from 'vue-google-oauth2'
+const gauthOption = {
+  clientId: '511458277120-n71rb8if053ge87h4vi29lksdkhm56or.apps.googleusercontent.com',
+  scope: 'profile email',
+  prompt: 'consent',
+  fetch_basic_profile: true
+}
+Vue.use(GAuth, gauthOption)
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+Vue.component('b-icon', BIcon)
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+  render: h => h(App),
 }).$mount('#app')
